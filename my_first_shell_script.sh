06be6a13ca2500b5af70cfbd3e4e5d33f9cb29dd
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2025-04-21 17:08:36 -0700

    chore: updates comments on the file

--- my_first_shell_script.sh
+++ my_first_shell_script.sh
@@ -10,4 +10,12 @@ sudo useradd user1
 sudo useradd user2
 sudo useradd user3
 
+# This is a single-line comment in Bash
+echo "Hello, you are learning Bash Scripting on DAREY.IO!" # This is also a comment, following a command
+
+# This is another way to create
+# a multi-line comment. Each line
+# is prefixed with a # symbol.
+echo "Here is an actual code that gets executed"
+
 
